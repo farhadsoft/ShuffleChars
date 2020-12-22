295bf357fba8dac8f6a55cb318f1fc61aa2daed0
Farhad <epam@farhad.su>
2020-12-23 00:15:19 +0300

    All done!

--- ShuffleCharacters/StringExtension.cs
+++ ShuffleCharacters/StringExtension.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ShuffleCharacters
 {
@@ -14,7 +16,54 @@ namespace ShuffleCharacters
         /// <exception cref="ArgumentException">Count of iterations less than 0.</exception>
         public static string ShuffleChars(string source, int count)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(source) || string.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentException($"Source string is null or empty or white spaces. {nameof(source)}");
+            }
+
+            if (count <= 0)
+            {
+                throw new ArgumentException("Count of iterations less than 0.");
+            }
+
+            int index = 0;
+            string result = source;
+            int length = source.Length;
+            List<string> strList = new List<string>();
+            StringBuilder evenChar = new StringBuilder((length / 2) + 1);
+            StringBuilder oddChar = new StringBuilder((length / 2) + 1);
+
+            for (int a = 0; a < count; a++)
+            {
+                if (index == 0)
+                {
+                    for (int i = 0; i < length; i += 2)
+                    {
+                        evenChar.Append(result[i]);
+                        if (i + 1 < length)
+                        {
+                            oddChar.Append(result[i + 1]);
+                        }
+                    }
+
+                    result = string.Concat(evenChar.ToString(), oddChar.ToString());
+                    evenChar.Clear();
+                    oddChar.Clear();
+                    strList.Add(result);
+                }
+                else
+                {
+                    int needed = a % (index + 1);
+                    result = strList[needed];
+                }
+
+                if (index == 0 && result == source)
+                {
+                    index = a;
+                }
+            }
+
+            return result;
         }
     }
 }
